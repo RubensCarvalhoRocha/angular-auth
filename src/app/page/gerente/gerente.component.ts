80ae63a12ba45b62d089686d2cb3185e75fce02a
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-11-19 09:29:09 -0300

    initial commit

---
+++ src/app/page/gerente/gerente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerente',
+  standalone: true,
+  imports: [],
+  templateUrl: './gerente.component.html',
+  styleUrl: './gerente.component.css',
+})
+export class GerenteComponent {}
